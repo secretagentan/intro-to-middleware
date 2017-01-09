b068879f0f5be7aca893fd342d0ff820bbe728be
Phil Lamplugh <phillip.lamplugh@gmail.com>
2017-01-09 14:07:40 -0800

    add solution

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var fs = require('fs');
+var morgan = require('morgan');
+var favicon = require('serve-favicon');
 
 var app = express();
 
+// mounting middleware
+app.use(morgan('dev'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// built in express middleware. if a request URL matches a file in
+// public, serve it, otherwise call next
+app.use(express.static(__dirname + '/public'));
+
+// if public has an index.html this won't be served.
 app.get('/', function(req, res) {
-  fs.readFile('./index.html', 'utf-8', function(err, data){
-    if (err) throw err;
-    res.set('Content-Type', 'text/html');
-    res.send(data);
-  });
+  res.send("Hello from server.js");
+});
+
+app.get('/about', function(req, res){
+  // send back a status 302 location: '/bio.html'
+  // browser will do another request
+  res.redirect('/bio.html');
 });
 
 var port = 3000;
